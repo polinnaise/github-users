6526180868652f06bfda0172cdcf9daae0f3edc9
stranger <stranger@strangers-MacBook-Pro.local>
2020-08-03 17:10:37 +0200

    remove unused css + formatting

--- src/App.vue
+++ src/App.vue
@@ -13,27 +13,21 @@
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Didact+Gothic&family=Montserrat:wght@200&display=swap');
 
-/* body {
-    background: #F0F0F0;
-    margin: 0;
-    color: black;
-    font-family: 'Montserrat', sans-serif !important;
-} */
 :root {
-	--color1: #d7cfe8;
-	--color2: #9c89c6;
+  --color1: #d7cfe8;
+  --color2: #9c89c6;
 }
 
 body {
   align-items: center;
-	background-image: linear-gradient(to bottom right, var(--color1), var(--color2));
-	color:white;
-	justify-content: center;
+  background-image: linear-gradient(to bottom right, var(--color1), var(--color2));
+  color:white;
+  justify-content: center;
   display: flex;
-	flex-direction: column;
-	height: 100vh;
+  flex-direction: column;
+  height: 100vh;
   margin: 0;
-	text-align: center;
+  text-align: center;
   font-family: 'Montserrat', sans-serif !important;
 }
 
@@ -42,48 +36,40 @@ h1 {
     font-size: 43px;
 }
 
-.container small {
-	opacity: 0.7;
-}
-
-.container a {
-	color: #eee;
-}
-
 .circle {
-	background-image: linear-gradient(to top right, var(--color1), var(--color2));
-	border-radius: 50%;
-	position: absolute;
-	z-index: -1;
+  background-image: linear-gradient(to top right, var(--color1), var(--color2));
+  border-radius: 50%;
+  position: absolute;
+  z-index: -1;
 }
 
 .circle.small {
-	top: 200px;
-	left: 150px;
-	width: 100px;
-	height: 100px;
+  top: 200px;
+  left: 150px;
+  width: 100px;
+  height: 100px;
 }
 
 .circle.medium {
-	background-image: linear-gradient(to bottom left, var(--color1), var(--color2));
-	bottom: 10px;
-	left: 10px;
-	width: 200px;
-	height: 200px;
+  background-image: linear-gradient(to bottom left, var(--color1), var(--color2));
+  bottom: 10px;
+  left: 10px;
+  width: 200px;
+  height: 200px;
 }
 
 .circle.big {
-	top: -100px;
-	right: -50px;
-	width: 400px;
-	height: 400px;
+  top: -100px;
+  right: -50px;
+  width: 400px;
+  height: 400px;
 }
 
 .circle.big2 {
-	bottom: -500px;
-	right: -150px;
-	width: 800px;
-	height: 800px;
+  bottom: -500px;
+  right: -150px;
+  width: 800px;
+  height: 800px;
 }
 
 @media screen and (max-width: 2560px) and (min-width: 1900px) {
